8ad43320a92f95b22529f3247af2bd0eb87a7e80
StepanTaz <stepan.taz@outlook.com>
2018-02-24 23:08:22 +0100

    let's kill da ho

--- script.js
+++ script.js
@@ -19,7 +19,7 @@ var isMobile = {
         return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
     }
 }
-
+var objData = document.getElementById("pdf");
 function openNav() {
 		document.getElementById("navBar").style.width = "250px";
 		document.getElementById("navBar").style.fontSize = "2vmax";
@@ -28,186 +28,180 @@ function closeNav() {
 		document.getElementById("navBar").style.width = "0";
 }
 function summ_aard_HO() {
-		var objData = document.getElementById("pdf");
 		if (isMobile.any()) {
-        objData.setAttribute('data', 'https://docs.google.com/gview?embedded=true&url=https://samenvattingen.github.io/summ_aard_HO.pdf');
+        	objData.setAttribute("data", "https://docs.google.com/gview?embedded=true&url=https://samenvattingen.github.io/summ_aard_HO.pdf");
         }
         else {
-            objData.setAttribute('data', 'summ_aard_HO.pdf');
+            objData.setAttribute("data", "summ_aard_HO.pdf");
         }    
         closeNav();
-        close(aard);    
+        closeAard();    
 }
-function summ_bio() {
-		var objData = document.getElementById("pdf");
+function summ_bio() {		
 		if (isMobile.any()) {
-        objData.setAttribute('data', 'https://docs.google.com/gview?embedded=true&url=https://samenvattingen.github.io/summ_bio.pdf');
+        	objData.setAttribute("data", "https://docs.google.com/gview?embedded=true&url=https://samenvattingen.github.io/summ_bio.pdf");
         }
         else {
-            objData.setAttribute('data', 'summ_bio.pdf');
+            objData.setAttribute("data", "summ_bio.pdf");
         }    
         closeNav();
-        close(bio);    
+        closeBio();    
 }
-function summ_chem_atom() {
-		var objData = document.getElementById("pdf");
+function summ_chem_atom() {		
 		if (isMobile.any()) {
-        objData.setAttribute('data', 'https://docs.google.com/gview?embedded=true&url=https://samenvattingen.github.io/summ_chem_atom.pdf');
+        	objData.setAttribute("data", "https://docs.google.com/gview?embedded=true&url=https://samenvattingen.github.io/summ_chem_atom.pdf");
         }
         else {
-            objData.setAttribute('data', 'summ_chem_atom.pdf');
+            objData.setAttribute("data", "summ_chem_atom.pdf");
         }    
         closeNav();
-        close(chem);    
+        closeChem();    
 }
-function summ_chem_even() {
-		var objData = document.getElementById("pdf");
+function summ_chem_even() {		
 		if (isMobile.any()) {
-        objData.setAttribute('data', 'https://docs.google.com/gview?embedded=true&url=https://samenvattingen.github.io/summ_chem_even.pdf');
+        	objData.setAttribute("data", "https://docs.google.com/gview?embedded=true&url=https://samenvattingen.github.io/summ_chem_even.pdf");
         }
         else {
-            objData.setAttribute('data', 'summ_chem_even.pdf');
+            objData.setAttribute("data", "summ_chem_even.pdf");
         }    
         closeNav();
-        close(chem);    
+        closeChem();    
 }
 function summ_eng_u3_4() {
-		var objData = document.getElementById("pdf");
+		
 		if (isMobile.any()) {
-        objData.setAttribute('data', 'https://docs.google.com/gview?embedded=true&url=https://samenvattingen.github.io/summ_eng_u3_4.pdf');
+        	objData.setAttribute("data", "https://docs.google.com/gview?embedded=true&url=https://samenvattingen.github.io/summ_eng_u3_4.pdf");
         }
         else {
-            objData.setAttribute('data', 'summ_eng_u3_4.pdf');
+            objData.setAttribute("data", "summ_eng_u3_4.pdf");
         }    
         closeNav();
-        close(eng);    
+        closeEng();    
 }
 function summ_frans_pronom() {
-		var objData = document.getElementById("pdf");
+		
 		if (isMobile.any()) {
-        objData.setAttribute('data', 'https://docs.google.com/gview?embedded=true&url=https://samenvattingen.github.io/summ_frans_pronom.pdf');
+        	objData.setAttribute("data", "https://docs.google.com/gview?embedded=true&url=https://samenvattingen.github.io/summ_frans_pronom.pdf");
         }
         else {
-            objData.setAttribute('data', 'summ_frans_pronom.pdf');
+            objData.setAttribute("data", "summ_frans_pronom.pdf");
         }    
         closeNav();
-        close(frans);    
+        closeFrans();    
 }
 function summ_fys() {
-		var objData = document.getElementById("pdf");
+		
 		if (isMobile.any()) {
-        objData.setAttribute('data', 'https://docs.google.com/gview?embedded=true&url=https://samenvattingen.github.io/summ_fys.pdf');
+        	objData.setAttribute("data", "https://docs.google.com/gview?embedded=true&url=https://samenvattingen.github.io/summ_fys.pdf");
         }
         else {
-            objData.setAttribute('data', 'summ_fys.pdf');
+            objData.setAttribute("data", "summ_fys.pdf");
         }    
         closeNav();
-        close(fys);    
+        closeFys();    
 }
 function summ_gesch_strijd() {
-		var objData = document.getElementById("pdf");
+		
 		if (isMobile.any()) {
-        objData.setAttribute('data', 'https://docs.google.com/gview?embedded=true&url=https://samenvattingen.github.io/summ_gesch_strijd.pdf');
+        	objData.setAttribute("data", "https://docs.google.com/gview?embedded=true&url=https://samenvattingen.github.io/summ_gesch_strijd.pdf");
         }
         else {
-            objData.setAttribute('data', 'summ_gesch_strijd.pdf');
+            objData.setAttribute("data", "summ_gesch_strijd.pdf");
         }    
         closeNav();
-        close(gesch);    
+        closeGesch();    
 }
 function summ_wisk_complex() {
-		var objData = document.getElementById("pdf");
+		
 		if (isMobile.any()) {
-        objData.setAttribute('data', 'https://docs.google.com/gview?embedded=true&url=https://samenvattingen.github.io/summ_wisk_complex.pdf');
+        	objData.setAttribute("data", "https://docs.google.com/gview?embedded=true&url=https://samenvattingen.github.io/summ_wisk_complex.pdf");
         }
         else {
-            objData.setAttribute('data', 'summ_wisk_complex.pdf');
+            objData.setAttribute("data", "summ_wisk_complex.pdf");
         }    
         closeNav();
-        close(wisk);    
+        closeWisk();    
 }
 function summ_wisk_funct() {
-		var objData = document.getElementById("pdf");
+		
 		if (isMobile.any()) {
-        objData.setAttribute('data', 'https://docs.google.com/gview?embedded=true&url=https://samenvattingen.github.io/summ_wisk_funct.pdf');
+        	objData.setAttribute("data", "https://docs.google.com/gview?embedded=true&url=https://samenvattingen.github.io/summ_wisk_funct.pdf");
         }
         else {
-            objData.setAttribute('data', 'summ_wisk_funct.pdf');
+            objData.setAttribute("data", "summ_wisk_funct.pdf");
         }    
         closeNav();
-        close(wisk);    
+        closeWisk();    
 }
 function summ_wisk_gon() {
-		var objData = document.getElementById("pdf");
+		
 		if (isMobile.any()) {
-        objData.setAttribute('data', 'https://docs.google.com/gview?embedded=true&url=https://samenvattingen.github.io/summ_wisk_gon.pdf');
+        	$("#pdf").attr("data", "https://docs.google.com/gview?embedded=true&url=https://samenvattingen.github.io/summ_wisk_gon.pdf");
         }
         else {
-            objData.setAttribute('data', 'summ_wisk_gon.pdf');
+            $("#pdf").attr("data", "summ_wisk_gon.html");
         }    
         closeNav();
-        close(wisk);    
+        closeWisk();    
 }
 function home() {
 	if (isMobile.any()) {
-		objData = document.getElementById("pdf")
-		objData.setAttribute("data", "homeMobile.html");
-		}
-		else {
-		objData = document.getElementById("pdf")
-		objData.setAttribute("data", "home.html");
-		}
+		$("#pdf").attr("data", "homeMobile.html");
+	}
+	else {
+		$("#pdf").attr("data", "home.html");
+	}
 	closeNav();
 }
 function show(vak) {
 	if (vak === aard) {
-		$("#aard").fadeIn(300);
+		$("#aard").fadeIn(200);
 	}
 	if (vak === bio) {
-		$("#bio").fadeIn(300);
+		$("#bio").fadeIn(200);
 	}
 	if (vak === chem) {
-		$("#chem").fadeIn(300);
+		$("#chem").fadeIn(200);
 	}
 	if (vak === eng) {
-		$("#eng").fadeIn(300);
+		$("#eng").fadeIn(200);
 	}
 	if (vak === frans) {
-		$("#frans").fadeIn(300);
+		$("#frans").fadeIn(200);
 	}
 	if (vak === fys) {
-		$("#fys").fadeIn(300);
+		$("#fys").fadeIn(200);
 	}
 	if (vak === gesch) {
-		$("#gesch").fadeIn(300);
+		$("#gesch").fadeIn(200);
 	}
 	if (vak === wisk) {
-		$("#wisk").fadeIn(300);
+		$("#wisk").fadeIn(200);
 	}
 };
 function closeAard() {
-	$("#aard").fadeOut(300);
+	$("#aard").fadeOut(200);
 }
 function closeBio() {
-	$("#bio").fadeOut(300);
+	$("#bio").fadeOut(200);
 }
 function closeChem() {
-	$("#chem").fadeOut(300);
+	$("#chem").fadeOut(200);
 }
 function closeEng() {
-	$("#eng").fadeOut(300);
+	$("#eng").fadeOut(200);
 }
 function closeFys() {
-	$("#fys").fadeOut(300);
+	$("#fys").fadeOut(200);
 }
 function closeFrans() {
-	$("#frans").fadeOut(300);
+	$("#frans").fadeOut(200);
 }
 function closeGesch() {
-	$("#gesch").fadeOut(300);
+	$("#gesch").fadeOut(200);
 }
 function closeWisk() {
-	$("#wisk").fadeOut(300);
+	$("#wisk").fadeOut(200);
 }
 function mobile() {
 	if (isMobile.any()) {
@@ -225,21 +219,5 @@ function mobile() {
 			objData = document.getElementById("pdf");
 			objData.setAttribute("data", "homeMobile.html");
 		});
-		/*var popupContent = document.getElementsByClassName("popup_content");
-		var navList = document.getElementsByClassName("navList");
-		var home = document.getElementsByClassName("home");
-		var txt = document.getElementsByClassName("txt");
-		for (var i = 0; i < popupContent.length; i++) {
-			//alert("MOBILE");
-			popupContent[i].style.width = "80%";
-			popupContent[i].style.height = "80%";
-			popupContent[i].style.fontSize = "20px";
-			popupContent[i].style.marginTop = "10%";
-		}
-		for (var i = 0; i < navList.length; i++) {
-			navList[i].style.fontSize = "20px";
-		}
-		objData = document.getElementById("pdf")
-		objData.setAttribute("data", "homeMobile.html");*/
 	}
 };
