d886d87d9a778e9cf8f2b7f5e6782942a34224b4
StepanTaz <stepan.taz@outlook.com>
2018-02-25 14:27:32 +0100

    big boy kleine code

--- script.js
+++ script.js
@@ -20,29 +20,23 @@ var isMobile = {
     }
 }
 function openNav() {
-		document.getElementById("navBar").style.width = "250px";
-		document.getElementById("navBar").style.fontSize = "2vmax";
-}
+	$("#navBar").css({
+		"width": "250px",
+		"font-size": "2vmax"
+	});
+};
 function closeNav() {
-		document.getElementById("navBar").style.width = "0";
-}
+	$("#navBar").css({"width": "0"});
+};
 function openSumm(summ, mobileSumm) {
-		var objData = document.getElementById("pdf");
 		if (isMobile.any()) {
-        	objData.setAttribute("data", mobileSumm);
+        	$("#pdf").attr("data", mobileSumm);
         }
         else {
-            objData.setAttribute("data", summ);
+        	$("#pdf").attr("data", summ);
         }    
         closeNav();
-        closeAard();
-        closeBio();
-        closeChem();
-        closeEng();
-        closeFrans();
-        closeFys();
-        closeGesch();
-        closeWisk();   
+        closeAll();
 };
 function home() {
 	if (isMobile.any()) {
@@ -52,57 +46,27 @@ function home() {
 		$("#pdf").attr("data", "home.html");
 	}
 	closeNav();
-}
-function show(vak) {
-	if (vak === aard) {
-		$("#aard").fadeIn(200);
-	}
-	if (vak === bio) {
-		$("#bio").fadeIn(200);
-	}
-	if (vak === chem) {
-		$("#chem").fadeIn(200);
-	}
-	if (vak === eng) {
-		$("#eng").fadeIn(200);
-	}
-	if (vak === frans) {
-		$("#frans").fadeIn(200);
-	}
-	if (vak === fys) {
-		$("#fys").fadeIn(200);
-	}
-	if (vak === gesch) {
-		$("#gesch").fadeIn(200);
-	}
-	if (vak === wisk) {
-		$("#wisk").fadeIn(200);
-	}
+	closeAll();
 };
-function closeAard() {
-	$("#aard").fadeOut(200);
+function show(vak) {
+	$(vak).fadeIn(200);
+};
+function closeInner(vak) {
+	$(vak).fadeOut(200);
+};
+function closeAll() {
+	closeInner('#aard');
+	closeInner('#wisk'); 
+	closeInner('#bio');
+    closeInner('#chem');
+    closeInner('#eng');
+    closeInner('#frans');
+    closeInner('#fys');
+    closeInner('#gesch');
 }
-function closeBio() {
-	$("#bio").fadeOut(200);
-}
-function closeChem() {
-	$("#chem").fadeOut(200);
-}
-function closeEng() {
-	$("#eng").fadeOut(200);
-}
-function closeFys() {
-	$("#fys").fadeOut(200);
-}
-function closeFrans() {
-	$("#frans").fadeOut(200);
-}
-function closeGesch() {
-	$("#gesch").fadeOut(200);
-}
-function closeWisk() {
-	$("#wisk").fadeOut(200);
-}
+function close() {
+	closeAll();
+};
 function mobile() {
 	if (isMobile.any()) {
 		$(document).ready(function(){
@@ -115,9 +79,7 @@ function mobile() {
 			$(".navList").css({
 				"font-size": "20px"
 			});
-			$("pdf").attr("data", "homeMobile.html");
-			objData = document.getElementById("pdf");
-			objData.setAttribute("data", "homeMobile.html");
+			$("#pdf").attr("data", "homeMobile.html");
 		});
-	}
+	};
 };
